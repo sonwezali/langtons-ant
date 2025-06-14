b1230aa08f9edb79869acf3b26bbcbc774f37e2f
Ali Sonmez <al1.sonmez.mi@gmail.com>
2025-06-14 21:28:19 +0300

    add randomization to initialization

--- ant.c
+++ ant.c
@@ -1,41 +1,51 @@
 #include <raylib.h>
 #include <time.h>
+#include <stdlib.h>
 
 #define COLS 288
 #define ROWS 162
+#define MAX_NUM_ANTS 5
 
 int sq = 8;
 
 Color colors[] = {WHITE, BLACK};
 int grid[ROWS][COLS] = {0};  // 0: white, 1: black
 int directions[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-int currentDirection;  // ant's current direction index
-int antLoc[2];
+int currentDirection[MAX_NUM_ANTS] = {0};  // ants' current direction index
+int antLoc[MAX_NUM_ANTS][2];
+int n;  // number of ants
 
 void init() {
-    antLoc[0] = ROWS / 2, antLoc[1] = COLS / 2;
-    currentDirection = 0;
+    srand(time(NULL));
+    n = (int) ((rand() / (float) RAND_MAX) * MAX_NUM_ANTS) + 1; // number of ants random [1, 5]
+    for (int i = 0; i < n; i++) {
+        antLoc[i][0] = (int) ((rand() / (float) RAND_MAX) * ROWS);
+        antLoc[i][1] = (int) ((rand() / (float) RAND_MAX) * COLS);
+    }
 }
 
-void move() {
-    grid[antLoc[0]][antLoc[1]] = !grid[antLoc[0]][antLoc[1]];  // flip the color
-    antLoc[0] = (antLoc[0] + directions[currentDirection][0] + ROWS) % ROWS;
-    antLoc[1] = (antLoc[1] + directions[currentDirection][1] + COLS) % COLS;
+void move(int ant) {
+    int currentAntLoc[2] = {antLoc[ant][0], antLoc[ant][1]};
+    grid[currentAntLoc[0]][currentAntLoc[1]] = !grid[currentAntLoc[0]][currentAntLoc[1]];  // flip the color
+    antLoc[ant][0] = (currentAntLoc[0] + directions[currentDirection[ant]][0] + ROWS) % ROWS;
+    antLoc[ant][1] = (currentAntLoc[1] + directions[currentDirection[ant]][1] + COLS) % COLS;
 }
 
-void whiteSquare() {
-    currentDirection = (currentDirection + 1) % 4;  // 90° clockwise
-    move();
+void whiteSquare(int ant) {
+    currentDirection[ant] = (currentDirection[ant] + 1) % 4;  // 90° clockwise
+    move(ant);
 }
 
-void blackSquare() {
-    currentDirection = (currentDirection + 3) % 4;  // 90° counter-clockwise
-    move();
+void blackSquare(int ant) {
+    currentDirection[ant] = (currentDirection[ant] + 3) % 4;  // 90° counter-clockwise
+    move(ant);
 }
 
 void update() {
-    if (grid[antLoc[0]][antLoc[1]] == 0) whiteSquare();
-    else blackSquare();
+    for (int i = 0; i < n; i++) {
+        if (grid[antLoc[i][0]][antLoc[i][1]] == 0) whiteSquare(i);
+        else blackSquare(i);
+    }
 }
 
 void drawGrid(){
